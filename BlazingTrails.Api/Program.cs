be26b3994f3c434e93b3a8013df522ce49d43353
Iggy988 <igor.rodic1988@gmail.com>
2023-11-25 21:53:33 +0100

    Set app to run api projects port not client port

--- BlazingTrails.Api/Program.cs
+++ BlazingTrails.Api/Program.cs
@@ -5,21 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseBlazorFrameworkFiles();
+
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
