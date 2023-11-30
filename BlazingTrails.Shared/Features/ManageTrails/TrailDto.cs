03d3e5b410cbe44cf41e32a1606fa15f3a7b1f07
Iggy988 <igor.rodic1988@gmail.com>
2023-11-30 22:36:01 +0100

    Trail Validator

--- BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using FluentValidation;
 
 namespace BlazingTrails.Shared.Features.ManageTrails;
 public class TrailDto
@@ -14,6 +14,27 @@ public class TrailDto
     {
         public int Stage { get; set; }
         public string Description { get; set; } = "";
+    }
+}
+
+public class TrailValidator : AbstractValidator<TrailDto>
+{
+    public TrailValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a name");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
+        RuleFor(x => x.Location).NotEmpty().WithMessage("Please enter a location");
+        RuleFor(x => x.Length).GreaterThan(0).WithMessage("Please enter a length");
+        RuleForEach(x => x.Route).SetValidator(new RouteInstructionValidator());
+    }
+}
+
+public class RouteInstructionValidator : AbstractValidator<TrailDto.RouteInstruction>
+{
+    public RouteInstructionValidator()
+    {
+        RuleFor(x => x.Stage).NotEmpty().WithMessage("Please enter a stage");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
     }
 }
 
