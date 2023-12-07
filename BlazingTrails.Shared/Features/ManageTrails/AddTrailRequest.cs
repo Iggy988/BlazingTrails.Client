5973b0e65d0d9cb491be204c7b69389bec2d373d
Iggy988 <igor.rodic1988@gmail.com>
2023-12-07 22:19:28 +0100

    AddTrailRequest record

---
+++ BlazingTrails.Shared/Features/ManageTrails/AddTrailRequest.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace BlazingTrails.Shared.Features.ManageTrails;
+public record AddTrailRequest(TrailDto Trail): IRequest<AddTrailRequest.Response>
+{
+    //defines the address of the API endpoint for the request
+    public const string RouteTemplate = "/api/trails";
+
+    //defines the response data for the request.
+    public record Response(int TrailId);
+}
+//A validator for the request. This will be executed by the API to make sure the request datais valid.
+public class AddTrailRequestValidator : AbstractValidator<AddTrailRequest>
+{
+    public AddTrailRequestValidator()
+    {
+        //Specifies the TrailValidator as the validator for the Trail property
+        RuleFor(x => x.Trail).SetValidator(new TrailValidator());
+    }
+}
